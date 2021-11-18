7bdda53efe1e9c09ada1bf180a442c8d2d7b3e3e
Joe Adam <jmadam01@gmail.com>
2021-11-17 23:47:34 -0600

    Added the abilit to request multiple things from database as well as by ID

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -1,6 +1,7 @@
-const BookRepository = require('../repository/BookRepository');
+
 const Book = require('../models/Book');
 const sqlite = require('sqlite3');
+const { application } = require('express');
 
 exports.get_books = function(req,res){
 
@@ -16,19 +17,28 @@ exports.get_books = function(req,res){
        });
         
        db.serialize(()=>{
+
+            var books = Array();
     
-    
-           db.each('SELECT bookName as name, bookId as id, bookSummary as summary, bookPublisher as publisher, bookAuthor as author FROM Book' , (err,row)=>{
+           db.all('SELECT bookName as name, bookId as id, bookSummary as summary, bookPublisher as publisher, bookAuthor as author FROM Book' , (err,row)=>{
        
                if(err){
        
                    return console.error(err.message + err.Database, + err.sqlite);
     
                }
-               var nbook = new Book(row.id, row.name, row.author,row.publisher,row.summary)
-               res.send(JSON.stringify(nbook));
+
+               for (let index = 0; index < row.length; index++) {
+                var nbook = new Book(row[index].id, row[index].name, row[index].author,row[index].publisher,row[index].summary)
+                books.push(nbook);
+                nbook = null;
+               }
+
+               
+               res.send(JSON.stringify(books));
+               
            })
-         
+           
        });
     
        db.close((err) => {
@@ -42,14 +52,48 @@ exports.get_books = function(req,res){
 
 }
 
-exports.get_book = function(req,res){
+exports.get_book = function(req,res,next){
 
-    var value = new Book();
+    let db = new sqlite.Database('./data/Information.db',sqlite.OPEN_READWRITE, (err) => {
+
+        if (err) {
     
-    var book = value.GetBooksById(1)
-
-    res.send(book);
-
+            console.error(err.message + err.Database, + err.sqlite);
+        }
+    
+        console.log("Connection to database was succesful!")
+       
+       });
+        
+       db.serialize(()=>{
+    
+    
+           db.each('SELECT bookName as name, bookId as id, bookSummary as summary, bookPublisher as publisher, bookAuthor as author FROM Book WHERE bookId = ' + req.query.id, (err,row)=>{
+       
+               if(err){
+       
+                   return console.error(err.message + err.Database, + err.sqlite);
+    
+               }
+               var nbook = new Book(row.id, row.name, row.author,row.publisher,row.summary)
+               res.set({
+                'Content-Type': 'text/plain',
+                'Content-Length': '123',
+                'ETag': '12345'
+                });
+               res.send(JSON.stringify(nbook));
+           })
+         
+       });
+    
+       db.close((err) => {
+    
+           if (err) {
+               console.error(err.message);
+           }
+           console.log('Database connection closed!');
+      
+       });
 }
 
 exports.get_book_test = function(req,res){
